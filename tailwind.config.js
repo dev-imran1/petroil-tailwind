1cec92a53cfc17cdae81e128def47fcc5da0d5c6
Imran Hossain <imran.cit.bd@gmail.com>
2023-10-09 00:21:25 +0600

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@ tailwind.config = {
     theme: {
       extend: {
         colors: {
-          clifford: '#da373d',
-        }
+          'pri-color': '#da373d',
+          'pera-color':'#6C6C6C',
+          'black-color':'#282828',
+        },
+        fontSize: {
+          'big-font':'64px',
+          'mid-font':'48px',
+          'title-font':'36px',
+          'sub-font':'24px',
+          'small-font':'16px',
+          'pera-font':'16px',
+        },
+        spacing: {
+          '5px': '5px',
+        },
+        backgroundImage: {
+          'banner': "url('/img/hero-pattern.svg')",
+          'overlay': "rgba(0, 0, 0, 0.60)",
+        },
+        maxWidth: {
+          'container': '1144px',
+        },
+        fontFamily: {
+          'popins': "Poppins', sans-serif",
+        },
       }
     }
   }
